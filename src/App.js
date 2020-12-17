bb9f22881c5091fd57898887e4be722216a5841a
bhaden94 <bhaden94@yahoo.com>
2020-12-16 19:01:57 -0800

    bg slider stuff working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BackgroundSlideshow from 'react-background-slideshow'
+import beer from "./images/beer-min.jpg";
+import liquor from "./images/liquor-min.jpg";
+import wine from "./images/wine-min.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>header</h1>
+			</header>
+      <BackgroundSlideshow images={[ beer, liquor, wine ]} animationDelay={5000} />
+		</div>
+	);
 }
 
 export default App;
